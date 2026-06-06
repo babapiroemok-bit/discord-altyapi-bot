0f6ce37d76a45a6367ce719a89c5e035af9ed610
babapiroemok-bit <babapiroemok@gmail.com>
2026-06-06 15:45:59 +0300

    feat: add lonely-bot files

---
+++ komutlar/nsfw-pussy.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const DabiImages = require("dabi-images");
+const DabiClient = new DabiImages.Client();
+const data = require('quick.db');
+
+
+exports.run = async (client, message, args) => {
+if(message.channel.nsfw == false) return message.channel.send(new Discord.MessageEmbed().setTitle('Bir hata oldu!').setImage('https://cdn.discordapp.com/attachments/941759205977772114/942127232380436480/0bec72cc449a40a07792ca1f343100f1.png').setDescription(`${message.author} **Kanal __NSFW__ olarak aktif değil. Kontrol et!**`).setColor('#00001'))
+if(await data.fetch(`slm.${message.author.id}.${message.guild.id}.p`)) return message.channel.send(new Discord.MessageEmbed().setColor('#00001').setDescription(`**10 saniyenin altında hızlı kullanamazsın!**`));
+DabiClient.nsfw.real.random().then(json => {
+message.channel.send(new Discord.MessageAttachment(json.url));
+data.set(`slm.${message.author.id}.${message.guild.id}.p`, 'codare');
+setTimeout(() => {
+data.delete(`slm.${message.author.id}.${message.guild.id}.p`)
+}, 10000);
+})
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'pussy'
+};
