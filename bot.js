d8afbe88eab39e280748cac68a9ddbcf9decb63d
babapiroemok-bit <babapiroemok@gmail.com>
2026-06-06 14:47:48 +0300

    Add bot.js

---
+++ bot.js
@@ -0,0 +1,46 @@
+const { Client, GatewayIntentBits, Partials, Collection, REST, Routes } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+client.commands = new Collection();
+const commands = [];
+
+const komutlarPath = path.join(__dirname, "komutlar");
+const komutFiles = fs.readdirSync(komutlarPath).filter(f => f.endsWith(".js"));
+
+for (const file of komutFiles) {
+  const komut = require(path.join(komutlarPath, file));
+  client.commands.set(komut.data.name, komut);
+  commands.push(komut.data.toJSON());
+}
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client, commands));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.sendStatus(200));
+app.listen(process.env.PORT || 3000);
+
+client.login(process.env.TOKEN);
