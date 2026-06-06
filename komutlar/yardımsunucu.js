4d410d1bedeec891b62b5d767c15d16684a2b638
babapiroemok-bit <babapiroemok@gmail.com>
2026-06-06 15:47:21 +0300

    feat: add remaining files

---
+++ komutlar/yardımsunucu.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const data = require('quick.db');
+
+exports.run = async (client, message, args) => {
+  Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * this.length)];
+  }
+
+let images = ['https://cdn.discordapp.com/attachments/941759205977772114/942118231244681266/standard_3.gif'];
+message.channel.send(new Discord.MessageEmbed().setColor('#000001').setDescription(`**Sunucu Menüsüne Hoş Geldiniz 😇**
+\`\`\`${client.user.username} Kullanırken @Lonely rolünü en üstte tutunuz.\`\`\`
+
+✨ \`${client.ayarlar.prefix}oto-isim-sistem\`
+Sunucunuza giren kullanıcıya istediğiniz şekilde otomatik isim ile adlandırmış yapabilirsin
+ 
+📁 \`${client.ayarlar.prefix}komut-mesaj-sistem\`**
+Sunucunuz için istediğiniz kadar **komut & **cevap** oluşturabilirsin**
+
+❌ **\`${client.ayarlar.prefix}yasak-tag-sistem\`
+Sunucunuz için güvenliği arttırın
+
+❕ \`${client.ayarlar.prefix}fake-hesap-sistem\`**
+Sunucunuza giren kullanıcı __5 gün__ içerisin de hesabını oluşturmuş ise cezalı verir
+
+🐝 \`${client.ayarlar.prefix}kanal arındır\`**
+Sunucunuzda ki tüm **kanalları** silerek yardımcı olur (**Sunucu Sahibi**)
+
+🚀 \`${client.ayarlar.prefix}sunucu-tema-sistem\`**
+Topluluk bağışları ile sunucu şablonlarına sahip ol! **${client.ayarlar.prefix}sunucu-kur** kullanabilirsin**
+
+🐝 \`${client.ayarlar.prefix}rol arındır\`
+Sunucunuzda ki tüm **rolleri** silerek yardımcı olur (**Sunucu Sahibi**)
+
+**Müzik botu arıyorsan, Lonely Music tam senin istediğin gibi hizmetinde! » **[Tıkla](https://discord.com/oauth2/authorize?client_id=933838376237420654&scope=bot&permissions=0) 
+`).setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.discordapp.com/attachments/942130580030636043/942130826542456883/anime-animeboy-goth-gothicstyle-redeyes-laughing-dark-aesthetic-anime-boy-manga-comics-book-person-transparent-png-723215.png').setImage(images.random()))
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sv', 'server'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'sunucu'
+};
