c9e2528fdf99ceece53c957a12367c13af7ecacb
babapiroemok-bit <babapiroemok@gmail.com>
2026-06-06 15:46:01 +0300

    feat: add lonely-bot files

---
+++ komutlar/nsfw.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const data = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+message.channel.send(new Discord.MessageEmbed().setTitle(`🔞`).setDescription(`**.anal
+.4k
+.ass
+.pgif
+.hentai
+.pussy
+.thigh**`));
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'nsfw'
+};
